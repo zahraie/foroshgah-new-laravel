1f6c3ca975ea76e60f1a235ebb2555b7b90ee131
zahra sohrabi <zahra.sohrabi.1374@gmail.com>
2022-07-04 01:04:34 +0430

    AdminPanel Completed

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Admin
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('admin')->namespace('Admin')->group(function(){
+
+    Route::get('/',[AdminDashboardController::class ,'index'])->name('admin.home');
+});
+
